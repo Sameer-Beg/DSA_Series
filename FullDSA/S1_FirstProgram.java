fa0633e30982f53354900b6da6189754bf1cdee8
SameerBeg <sameer423j@gmail.com>
2025-08-14 19:56:06 +0530

    Add

---
+++ FullDSA/S1_FirstProgram.java
@@ -0,0 +1,16 @@
+package DSA_Series.FullDSA;
+
+import java.util.Scanner;
+
+public class S1_FirstProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number one hre..");
+        int a = sc.nextInt();
+        System.out.println("Enter the number two here..");
+        int b = sc.nextInt();
+        int c = a + b ;
+        System.out.println("The sum a + b is ::" + c);
+
+    }
+}

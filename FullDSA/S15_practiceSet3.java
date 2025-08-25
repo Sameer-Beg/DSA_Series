a25006161a6572e2aa502a6d3fc05b44541e8469
SameerBeg <sameer423j@gmail.com>
2025-08-25 16:42:50 +0530

    Addfiles

---
+++ FullDSA/S15_practiceSet3.java
@@ -0,0 +1,44 @@
+package DSA_Series.FullDSA;
+
+import java.util.Scanner;
+
+public class S15_practiceSet3 {
+    public static void main(String[] args) {
+//        area of circle
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter radius: ");
+//        int r = sc.nextInt();
+//        double p = 3.14159;
+//        double area = p * r * r;
+//        System.out.println("Area of Circle = " + area);
+
+//        area of tringle
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter.");
+//        int a = sc.nextInt();
+//        int h = sc.nextInt();
+//        double area = 0.5 * a * h ;
+//        System.out.println(area);
+
+//        area of ractangle
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Entere,,");
+//        int l = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(l * b);
+
+        //area of pallelogram
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter height: ");
+        double height = sc.nextDouble();
+        double area = base * height;   // formula: base × height
+        System.out.println("Area of Parallelogram = " + area);
+
+
+
+
+
+    }
+}

a88d2d0eb3ccf4e8089bf15cc0235080255035bf
SameerBeg <sameer423j@gmail.com>
2025-08-28 16:07:40 +0530

    Add

--- FullDSA/S24_MethodOverloading.java
+++ FullDSA/S24_MethodOverloading.java
@@ -7,6 +7,7 @@ public class S24_MethodOverloading {
     //Method1
     static int sum (int a , int b){
         int c = a + b ;
+        System.out.println(c);
         return c;
     }
     //Method2
@@ -15,10 +16,13 @@ public class S24_MethodOverloading {
         return d;
     }
     public static void main(String[] args) {
-  int ans =    sum(20,30);
-        System.out.println(ans);
+//        int ans =    sum(20,30);
+//        System.out.println(ans);
+        sum(3,5);
 
-    int ans2 = sum(20,30,30);
-        System.out.println(ans2);
+    /*int ans2 = sum(20,30,30);
+        System.out.println(sum(20,30));*/
     }
 }
+
+

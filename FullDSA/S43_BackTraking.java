ebd1a069b5be3f31860aea25df4cdf3655b1d659
SameerBeg <sameer423j@gmail.com>
2025-11-01 16:44:34 +0530

    add

---
+++ FullDSA/S43_BackTraking.java
@@ -0,0 +1,22 @@
+package DSA_Series.FullDSA;
+
+public class S43_BackTraking {
+    public static void printPermutation(String str , String perm, int index ){
+        if(str.length() ==0){
+            System.out.println(perm);
+            return;
+        }
+        for(int i = 0 ; i<str.length(); i++){
+            char Currentchar = str.charAt(i);
+            String newStr = str.substring(0 , i) + str.substring(i+1);
+            printPermutation(newStr, perm + Currentchar , index + 1);
+
+
+        }
+
+    }
+    public static void main(String[] args) {
+        String str = "ABC";
+        printPermutation(str , " " , 0);
+    }
+}
